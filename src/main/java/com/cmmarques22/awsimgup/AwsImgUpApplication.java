a7aac1e59dfbd43a00efca73cfc0e3a2603bb7d4
cmmarques22 <carlos.marques22@live.com>
2023-11-03 18:13:47 +0000

    first commit

---
+++ src/main/java/com/cmmarques22/awsimgup/AwsImgUpApplication.java
@@ -0,0 +1,13 @@
+package com.cmmarques22.awsimgup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImgUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImgUpApplication.class, args);
+	}
+
+}
