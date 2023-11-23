f1bc676e1c478b3b0c395d83e27442243f7254e0
cmmarques22 <carlos.marques22@live.com>
2023-11-23 17:40:23 +0000

    update userprofiledata

--- src/main/java/com/cmmarques22/awsimgup/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/cmmarques22/awsimgup/datastore/FakeUserProfileDataStore.java
@@ -4,9 +4,16 @@ import com.cmmarques22.awsimgup.profile.UserProfile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
-
+    static {
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "carlosmarques", null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "manuelcalavera", null));
+    }
+    public List<UserProfile> getUserProfiles() {
+        return USER_PROFILES;
+    }
 }
